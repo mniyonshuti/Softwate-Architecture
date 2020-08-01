2f26f29053caa40b01bb14773b90e917d504159c
Moses Niyonshuti <mniyonshuti@Mosess-MBP.attlocal.net>
2020-08-01 18:37:44 -0500

    Added lab 5

---
+++ Lab5/uwera/src/main/java/com/example/uwera/UweraApplication.java
@@ -0,0 +1,13 @@
+package com.example.uwera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UweraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UweraApplication.class, args);
+    }
+
+}
---
+++ Lab5/uwera/src/test/java/com/example/uwera/UweraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uwera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UweraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
