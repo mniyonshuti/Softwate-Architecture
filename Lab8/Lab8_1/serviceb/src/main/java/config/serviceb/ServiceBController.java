4cd3821a6edae4480d5ba521d2259f1b4c9bd78a
Moses Niyonshuti <mniyonshuti@Mosess-MBP.attlocal.net>
2020-08-22 00:26:19 -0500

    lab 8

---
+++ Lab8/Lab8_1/serviceb/src/main/java/config/serviceb/ServiceBController.java
@@ -0,0 +1,15 @@
+package config.serviceb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceBController {
+    @Value("${greeting}")
+    private String message;
+    @RequestMapping("/")
+    public String getName() {
+        return message;
+    }
+}
